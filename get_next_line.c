d0e56d3f4811f0d85637909ca95ff287e56a4768
Daniel Reule <dreule@2-H-6.42heilbronn.de>
2024-10-29 11:29:47 +0100

    ADD(strchr, strdup, strlcat)

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,120 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 07:59:13 by dreule            #+#    #+#             */
-/*   Updated: 2024/10/28 08:31:27 by dreule           ###   ########.fr       */
+/*   Updated: 2024/10/29 11:23:36 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strchr_gnl(const char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if ((char)c == '\0')
+		return ((char *)str);
+	return (NULL);
+}
+
+char	*ft_strdup_gnl(const char *s1)
+{
+	char		*store;
+	size_t		i;
+	int			len;
+	const char	*temp;
+
+	i = 0;
+	len = 0;
+	temp = s1;
+	while (*temp != '\0')
+	{
+		temp++;
+		len++;
+	}
+	store = malloc(len + 1);
+	if (!store)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		store[i] = s1[i];
+		i++;
+	}
+	store[i] = '\0';
+	return (store);
+}
+
+char	*extract_line(char **leftovers)
+{
+	char	*ex_line;
+	char	*line_pos;
+	char	*temp_letfovers;
+
+	if (*leftovers == NULL)
+		return (NULL);
+	line_pos = ft_strchr_gnl(*leftovers, '\n');
+	if (line_pos)
+	{
+		*(line_pos + 1) = '\0';
+		ex_line = ft_strdup_gnl(*leftovers);
+		temp_letfovers = ft_strdup_gnl(line_pos + 1);
+		free(*leftovers);
+		*leftovers = temp_letfovers;
+	}
+	else
+	{
+		ex_line = ft_strdup_gnl(*leftovers);
+		free(*leftovers);
+		*leftovers = NULL;
+	}
+	return (ex_line);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+	size_t	j;
+
+	dst_len = 0;
+	src_len = 0;
+	while (dst[dst_len] != '\0' && dst_len < dstsize)
+		dst_len++;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	i = dst_len;
+	j = 0;
+	while (src[j] != '\0' && (i + 1) < dstsize)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (dst_len + src_len);
+}
+
 char	*get_next_line(int fd)
 {
-	
+	static char	*leftovers;
+	char		*found_line;
+	char		*buffer;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	found_line = extract_line(&leftovers);
+	if (found_line)
+		return (found_line);
+	buffer = malloc(sizeof(BUFFER_SIZE + 1));
+	buffer = read(fd, buffer, BUFFER_SIZE + 1);
+	if (buffer == -1)
+		return (NULL);
+	leftovers = NULL;
+	return (leftovers);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,23 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 07:59:56 by dreule            #+#    #+#             */
-/*   Updated: 2024/10/28 08:50:26 by dreule           ###   ########.fr       */
+/*   Updated: 2024/10/29 11:23:48 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
 
 # include <unistd.h>
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+char	*ft_strdup_gnl(const char *s1);
+char	*ft_strchr_gnl(const char *str, int c);
+char	*extract_line(char **leftovers);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif
