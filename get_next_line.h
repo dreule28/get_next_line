a70c1582f3ccacd36e8d79a3faab12540ea0bdae
Daniel Reule <dreule@2-H-6.42heilbronn.de>
2024-10-30 16:02:27 +0100

    CHNG(switched strlcpy in headerfile)

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dreule <dreule@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 07:59:56 by dreule            #+#    #+#             */
-/*   Updated: 2024/10/30 11:53:19 by dreule           ###   ########.fr       */
+/*   Updated: 2024/10/30 16:01:34 by dreule           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ char	*extract_line(char **leftovers);
 char	*ft_strcat_gnl(char *dest, char *src);
 size_t	ft_strlen_gnl(const char *str);
 char	*ft_strjoin_gnl(char const *s1, char const *s2);
-char	*ft_strcpy_gnl(char *dest, char *src);
+size_t	ft_strlcpy_gnl(char *dst, const char *src, size_t dstsize);
 
 #endif
